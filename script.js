30ae17a1b8ce23b8f53799dbf8570dcd8b65b83a
Manjunath <66263712+Manjunath1299@users.noreply.github.com>
2023-12-17 13:40:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,17 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
-  
+	let totacost=0;
+  arr=arr.sort((a,b)=>b-a);
+	while (arr.length>1){
+		let currentcost=arr[arr.length-1]+arr[arr.length-2];
+		total+=totacost;
+		arr.pop();
+		arr.pop();
+		arr.push(currentcost);
+		arr.sort((a,b)=>b-a);
+	}
+	console.log(totacost);
 }
 
 module.exports=mincost;
